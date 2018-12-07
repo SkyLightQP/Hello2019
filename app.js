e66660ec26e0fba35b9f74b974b7dc11176e7aaa
SkyLightQP <combbm@naver.com>
2018-12-07 20:55:17 +0900

    초기 셋팅

---
+++ app.js
@@ -0,0 +1,22 @@
+const logger = require('log4js').getLogger();
+const app = require('express')();
+
+const { Nuxt, Builder } = require('nuxt');
+const nuxtConfig = require('./nuxt.config');
+
+if (process.env.NODE_ENV === 'production') {
+    logger.level = 'ALL';
+} else if (process.env.NODE_ENV === 'development') {
+    logger.level = 'DEBUG';
+    nuxtConfig.dev = true;
+}
+
+const nuxt = new Nuxt(nuxtConfig);
+
+if (nuxtConfig.dev) {
+    new Builder(nuxt).build();
+}
+app.use(nuxt.render);
+
+const port = 80;
+app.listen(port, () => logger.info(`Http server listening on ${port}`));
---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    loading: {
+        color: 'yellow'
+    },
+
+    head:{
+        title: '안녕! 2019!',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ]
+    }
+}
